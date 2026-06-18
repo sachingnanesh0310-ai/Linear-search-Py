6b3f440e7bf0508a9a44b476ecfc6fc122dee5be
Sachin_0310 <sachingnanesh0310@gmail.com>
2026-06-18 08:31:26 +0530

    Add files via upload

---
+++ linear.py
@@ -0,0 +1,14 @@
+arr = [10, 25, 30, 45, 50]
+key = 30
+
+position = -1
+
+for i in range(len(arr)):
+    if arr[i] == key:
+        position = i
+        break
+
+if position != -1:
+    print("Element found at index:", position)
+else:
+    print("Element not found")
